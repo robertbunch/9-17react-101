2a215f18529372fef14eec3b7972a3524d7c1e4a
Robert Bunch <rdbunch@gmail.com>
2019-09-04 15:24:22 -0400

    data.js challenge

--- Card.js
+++ Card.js
@@ -4,19 +4,21 @@ function Card(props){
     const name = "Robert Bunch";    
     // const title = "React From the Beginning";
     // const title = props.title;
-    const saleOn = false;
+    // const saleOn = false;
+    // const props.name = /* BAD!!! LIKE THE NO SAINTS */
+
     return (
             <div className="col s2">
                 <div className="card hoverable small">
                     <div className="card-image">
-                        {/* <img src={props.data.image} /> */}
+                        <img src={props.img} />
                     </div>
                     <div className="card-content">
-                        {/* <p>{props.data.course}</p> */}
-                        {/* <p>{props.data.instructor}</p> */}
+                        <p>{props.title}</p>
+                        <p>{props.name}</p>
                     </div>
                     <div className="card-action">
-                        <a href="#">${saleOn ? 9.99 : 59.99}</a>
+                        <a href="#">${props.saleOn ? 9.99 : 59.99}</a>
                     </div>
                 </div>
             </div>	
--- reactComponents.js
+++ reactComponents.js
@@ -1,10 +1,12 @@
 ReactDOM.render(
-    <div>
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
+    <div className="container">
+        <div className="row">
+            <Card name={data[0].instructor} saleOn={true} title={data[0].course} img={data[0].image} />
+            <Card name={data[1].instructor} saleOn={true} title={data[1].course} img={data[1].image} />
+            <Card name="Ian" title="CornStalk Man" />
+            <Card name="Tracy" title="Pho/Ramen Foodie" />
+            <Card name="Tony" title="Man amongst toads"/>
+        </div>
     </div>,
     document.querySelector('#root')
 )
